19936a24e33b90ef4386bc4d6cf236c1e4966e08
Yoann Cerda <tuxlinuxien@gmail.com>
2016-08-20 16:18:28 +0800

    Get username when user register from github.

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from user_management.models import UserExtend
 
-# Register your models here.
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class UserExtendInline(admin.StackedInline):
+    model = UserExtend
+    can_delete = False
+    verbose_name_plural = 'extended'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserExtendInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+class UserExtend(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    timezone = models.CharField(
+        verbose_name=_('timezone'),
+        max_length= 50,
+        blank=True,
+        null=True,
+    )
