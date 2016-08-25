f0e8d8e26eaab419d3097fabc02baf282d0787cf
Julien Buty <julien@nepsilon.net>
2016-08-25 14:05:56 +0800

    Change template name to use hyphens.

--- subscriber_management/urls.py
+++ subscriber_management/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url, include
 from subscriber_management import views
 
 urlpatterns = [
-    url(r'^$',views.SubscriberListView.as_view(), name='subscriber_management_list'),
-    url(r'^list$',views.SubscriberListView.as_view(), name='subscriber_management_list'),
-    url(r'^list/(?P<uuid>[^/]+)/$',views.SubscriberListSubscribersView.as_view(), name='subscriber_management_list_subscribers'),
-    url(r'^list/(?P<uuid>[^/]+)/delete$',views.SubscriberListDeleteView.as_view(), name='subscriber_management_list_delete'),
-    url(r'^list/(?P<uuid>[^/]+)/join$',views.SubscribeJoin.as_view(), name='subscriber_management_join'),
-    url(r'^create$',views.SubscriberCreateView.as_view(), name='subscriber_management_create'),
+    url(r'^$',views.SubscriberListView.as_view(), name='subscriber-management-list'),
+    url(r'^list$',views.SubscriberListView.as_view(), name='subscriber-management-list'),
+    url(r'^list/(?P<uuid>[^/]+)/$',views.SubscriberListSubscribersView.as_view(), name='subscriber-management-list-subscribers'),
+    url(r'^list/(?P<uuid>[^/]+)/delete$',views.SubscriberListDeleteView.as_view(), name='subscriber-management-list-delete'),
+    url(r'^list/(?P<uuid>[^/]+)/join$',views.SubscribeJoin.as_view(), name='subscriber-management-join'),
+    url(r'^create$',views.SubscriberCreateView.as_view(), name='subscriber-management-create'),
 ]
