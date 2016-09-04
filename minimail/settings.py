eb594a3670b05a3aea62dfbeab868806f5835782
Julien Buty <julien@nepsilon.net>
2016-09-04 17:53:01 +0800

    Add STATIC_ROOT in settings.

--- minimail/settings.py
+++ minimail/settings.py
@@ -153,9 +153,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR+'/static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
 ]
 
 SITE_ID = 1
