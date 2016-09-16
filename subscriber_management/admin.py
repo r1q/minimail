057c53440c5fc54f0e2bc7345ce2b0ccf14d42e0
Yoann Cerda <tuxlinuxien@gmail.com>
2016-09-16 19:51:39 +0800

    Add admin for List and Subscriber

--- subscriber_management/admin.py
+++ subscriber_management/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from subscriber_management.models import Subscriber, List
 
-# Register your models here.
+admin.site.register(Subscriber)
+admin.site.register(List)
