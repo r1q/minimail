efcfda47895e87786fdeddf4cf81402379daa018
Yoann Cerda <tuxlinuxien@gmail.com>
2016-09-16 19:59:13 +0800

    Add admin.

--- subscriber_management/admin.py
+++ subscriber_management/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from subscriber_management.models import Subscriber, List
 
-admin.site.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'list', 'validated')
+    search_fields = ('email',)
+
+admin.site.register(Subscriber, SubscriberAdmin)
 admin.site.register(List)
