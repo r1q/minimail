c233c1627960ab058381eea8edd31722c9d7c474
Yoann Cerda <tuxlinuxien@gmail.com>
2016-10-16 14:41:01 +0800

    Update settings page redirect_login

--- subscriber_management/admin.py
+++ subscriber_management/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from subscriber_management.models import Subscriber, List
 
 class SubscriberAdmin(admin.ModelAdmin):
-    list_display = ('email', 'list', 'validated')
+    list_display = ('email', 'list', 'validated',)
     search_fields = ('email',)
 
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'from_email', 'from_email_verified',)
+    search_fields = ('name',)
+
 admin.site.register(Subscriber, SubscriberAdmin)
-admin.site.register(List)
+admin.site.register(List, ListAdmin)
