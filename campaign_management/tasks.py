bd03ca9c7fc910277b04967609a58c4654fa47b6
Julien Buty <julien@nepsilon.net>
2016-11-27 11:59:40 +0800

    Add pixel for tracking open.

--- campaign_management/tasks.py
+++ campaign_management/tasks.py
@@ -20,10 +20,19 @@ def _inject_unsubscribe_link(subscriber, campaign):
     return html_email, text_email, unsubscribe_link
 
 
-def _gen_campaign_emails(campaign_id, list_id):
-    """_make_newsletter_emails
+def _inject_tracking_pixel(subscriber, email_list, campaign):
+    pixel_url = "http://minimail.fullweb.io/pixou/open/?list={}&campaign={}&subscriber={}"\
+                .format(email_list.uuid, campaign.uuid, subscriber.uuid)
+    tracking_snippet = '<img src="{}" height=1 width=1 /></body'.format(pixel_url)
+    html_email = campaign.html_email_for_sending.replace('</body', tracking_snippet)
+    return html_email
 
-    :param campaign:
+
+def _gen_campaign_emails(campaign_id, list_id):
+    """_gen_campaign_emails
+
+    :param campaign_id:
+    :param list_id:
     """
     try:
         campaign = Campaign.objects.get(pk=campaign_id)
@@ -37,28 +46,32 @@ def _gen_campaign_emails(campaign_id, list_id):
         email_headers['X-Mailer'] = "Minimail Mailer"
         email_headers['List-ID'] = "MINIMAIL-{}".format(email_list.uuid)
         email_headers['X-Campaign-Id'] = "MINIMAIL-{}".format(campaign.uuid)
+        # Format sender field
+        _from = "{} <{}>".format(campaign.email_from_name,
+                                 campaign.email_list.from_email)
 
     for subscriber in subscribers:
         try:
             # TODO: Remember at what byte offset we inject the  unsubscribe_link,
             #       to prefer string O(n) search each time
+            # Add custom unsubscribe link for this subscriber
             html_email, text_email, unsubscribe_link = _inject_unsubscribe_link(subscriber,
                                                                                 campaign)
-            # TODO: Inject tracking pixel
-            _from = "{} <{}>".format(campaign.email_from_name,
-                                     campaign.email_list.from_email)
+            # Add custom tracking open pixel for this subscriber
+            html_email = _inject_tracking_pixel(subscriber, email_list, campaign)
             # Build current email headers
             curr_email_headers = {}
             curr_email_headers['List-Unsubscribe'] = "<{}>".format(unsubscribe_link)
             curr_email_headers['X-Recipient-ID'] = "{}".format(subscriber.uuid)
             curr_email_headers.update(email_headers)
+            # Create email message object
             email = EmailMultiAlternatives(campaign.email_subject,  # email subject
                                            text_email,  # body text version
                                            _from,  # from sender
                                            [subscriber.email],  # recipient
                                            reply_to=[campaign.email_reply_to_email],
                                            headers=curr_email_headers)
-            email.attach_alternative(html_email, "text/html")
+            email.attach_alternative(html_email, "text/html")  # adding html email
         except Exception as ex:
             print(ex)
             email = False
