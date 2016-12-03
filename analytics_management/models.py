d24850a895dbafb66966deef432f1e09f6257d3e
Yoann Cerda <tuxlinuxien@gmail.com>
2016-12-03 19:04:17 +0800

    Update open rate based on delivery count.

--- analytics_management/models.py
+++ analytics_management/models.py
@@ -2,20 +2,23 @@ from django.db import models
 from analytics_management.models_pixou import \
     PixouOpenRate, PixouClickRate, PixouSesRate
 
+def _get_ses_stats(list_uuid:str, campaign_uuid:str):
+    return SesRate.objects.using('pixou').get(
+        list=list_uuid,
+        campaign=campaign_uuid,
+    )
+
 # Create your models here.
 class OpenRate(PixouOpenRate):
 
     def avg(self):
-        if self.total == 0:
+        ses_stats = _get_ses_stats(self.list, self.campaign)
+        if ses_stats.delivery == 0:
             return 0
-        return int(self.uniq / self.total)*100
+        return int(self.uniq / ses_stats.delivery)*100
 
 class ClickRate(PixouClickRate):
-
-    def avg(self):
-        if self.total == 0:
-            return 0
-        return int(self.uniq / self.total)*100
+    pass
 
 class SesRate(PixouSesRate):
     pass
