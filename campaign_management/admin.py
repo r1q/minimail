9397c37f1b7e073adcfd0131c3e1a923d0273b45
Julien Buty <julien@nepsilon.net>
2016-12-11 10:55:35 +0800

    Update Admin listing column.

--- campaign_management/admin.py
+++ campaign_management/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from campaign_management.models import Campaign
 
 class CampaignAdmin(admin.ModelAdmin):
-    list_display = ('name','uuid', 'email_list', 'author',)
+    list_display = ('email_subject','is_sent', 'email_list', 'author',)
     readonly_fields = ('uuid',)
 
 admin.site.register(Campaign, CampaignAdmin)
