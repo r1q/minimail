c84c8154669e3ca4af2e872cbd88b4d854e08ee7
Yoann Cerda <tuxlinuxien@gmail.com>
2016-12-16 12:41:15 +0800

    Fix uri base64 decode.

--- analytics_management/templatetags/filters_extra.py
+++ analytics_management/templatetags/filters_extra.py
@@ -6,7 +6,10 @@ register = template.Library()
 @register.filter(name='decodeURI')
 def decodeURI(value):
     try:
-        return base64.standard_b64decode(value).encode("utf-8")
+        decoded_uri = base64.standard_b64decode(value)
+        if len(decoded_uri) <= 37:
+            return decoded_uri
+        return decoded_uri[:37]+bytes("...","utf-8")
     except Exception as err:
         print(err)
         return "<invalid URI> "+value
